219dac885e5c991857bab3a3b4918001cffd98a4
Sriya-Kandhala <sriyareddy496@gmail.com>
2025-12-10 09:16:17 +0530

    Make backend Vercel serverless compatible

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running on Vercel ✅");
+});
+
 app.use("/api/v1/products", router);
 app.use("/images", express.static("images"));
 
-// const MONGO_URL = "mongodb+srv://sriyareddy496_db_user:tocEJeLfo8DPj1Hg@cluster0.6rdmffw.mongodb.net/?appName=Cluster0";
  
 
 mongoose.connect(process.env.MONGO_URL)
